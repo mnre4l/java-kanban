e2955dba035c30da751b1fbc1d42530f8c74b9fe
Ivan Kotov <spicet@mail.ru>
2023-04-05 22:22:26 +0300

    <style>: убрать неиспользованные импорты

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private final Map<Integer, Node> historyMap = new HashMap<>();
